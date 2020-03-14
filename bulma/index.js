3b2f97cb153d5bcaba700870e4cdea7070a4a543
mishu <mishu@xtelecom.ro>
2020-03-14 19:01:24 +0200

    fixes export

--- bulma/index.js
+++ bulma/index.js
@@ -1,4 +1,4 @@
 import CheckboxManager from '../src/bulma/CheckboxManager.vue';
 import CheckboxItems from '../src/bulma/CheckboxItems.vue';
 
-export default { CheckboxManager, CheckboxItems };
+export { CheckboxManager, CheckboxItems };
